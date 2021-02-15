83e1c82b2a07073dd365e21bf2c607931b66046b
Larry Jenkins <75516987+larrygjenkins@users.noreply.github.com>
2021-02-14 16:44:04 -0800

    added IDs to time-block divs for past, present, future function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var currentDate = moment().format("dddd, MMMM DD, YYYY");
 
 console.log("now = " + currentDate);
 
+// This function displays the current date at the top of the planner.
 function setDate() {
     // var displayDate = $("#currentDay");
     var displayDate = document.getElementById("currentDay");
@@ -9,3 +10,13 @@ function setDate() {
 }
 
 setDate();
+
+
+var currentHour = moment().format("hA");
+
+console.log("current hour is " + currentHour);
+
+
+// function pastPresentFuture () {
+//     if 
+// }
