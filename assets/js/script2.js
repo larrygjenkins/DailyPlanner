b9344a22bc4ee7a06f9b9e756fd3b8e42e8d9dfa
Larry Jenkins <75516987+larrygjenkins@users.noreply.github.com>
2021-02-23 22:43:40 -0800

    removed unnecessary html and js files that had been used for testing

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,7 +1,5 @@
 var currentDate = moment().format("dddd, MMMM DD, YYYY");
 
-console.log("now = " + currentDate);
-
 // This function displays the current date at the top of the planner.
 function setDate() {
     var displayDate = document.getElementById("currentDay");
